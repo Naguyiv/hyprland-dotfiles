2fea7a76b7568bac81db270a0dad4d1b7843dc53
Jesse Akinwumi <naguyiv@gmail.com>
2026-08-18 22:21:45 -0400

    Modify Install.sh
    Add linux-lts kernel via pacman

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sudo pacman -S hyprland  hyprlock hypridle \
-quickshell qt6-5compat \
-firefox brightnessctl alacritty grim slurp 
+#sudo pacman -S hyprland  hyprlock hypridle \
+#quickshell qt6-5compat \
+#firefox brightnessctl alacritty grim slurp 
 
-sudo pacman -S sof-firmware alsa-firmware alsa-utils pipewire-alsa pipewire-audio pipewire wireplumber rtkit alsa-utils 
-sudo systemctl enable --now rtkit-daemon && systemctl --user enable --now wireplumber 
+#sudo pacman -S sof-firmware alsa-firmware alsa-utils pipewire-alsa pipewire-audio pipewire wireplumber rtkit alsa-utils 
+#sudo systemctl enable --now rtkit-daemon && systemctl --user enable --now wireplumber 
+
+sudo pacman -S hyprland hyprlock hypridle quickshell qt6-5compat \
+	       firefox brightnessctl alacritty grim slurp wl-clipboard \
+	       sof-firmware alsa-firmware alsa-utils pipewire-alsa pipewire-audio pipewire \
+	       wireplumber rtkit bluez-utils bluez linux-lts linux-lts-headers 
+
+sudo systemctl enable --now rtkit-daemon bluetooth && systemctl --user enable --now wireplumber pipewire
